0964f7e63b1a9bbf4ef6467acada98c0b87af869
梁承樸 <chengpuliang@gmail.com>
2024-03-20 00:09:36 +0800

    新增喜愛書籍、最近閱讀顯示區，新增閱讀器頁數跳轉問題

---
+++ Komiic/ComicHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  ComicHistoryView.swift
+//  Komiic
+//
+//  Created by 梁承樸 on 2024/3/19.
+//
+
+import SwiftUI
+
+struct ComicHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComicHistoryView()
+}
---
+++ Komiic/FavoritesComicView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesComicView.swift
+//  Komiic
+//
+//  Created by 梁承樸 on 2024/3/19.
+//
+
+import SwiftUI
+
+struct FavoritesComicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesComicView()
+}
